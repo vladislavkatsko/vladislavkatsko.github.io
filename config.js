600f3ee93de2bbe84e76040c7ae0b11e6957f92f
vladislavkatsko <vladislav.katsko@gmail.com>
2025-05-22 20:48:46 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+// config.js — здесь можно легко менять контент сайта
+
+window.siteConfig = {
+    name: "Владислав Катсько",
+    subtitle: "Фотограф / Визуальный художник",
+    email: "example@email.com",
+    gallery: [
+        "images/photo1.jpg",
+        "images/photo2.jpg",
+        "images/photo3.jpg"
+    ]
+};
